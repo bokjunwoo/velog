f2a973b4acffef4e4c078a923505f4d674893ac1
JUNU <bokjunwoo@gmail.com>
2023-02-26 22:49:14 +0900

    refactor : Axios interceptors 함수 분리

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,8 +7,11 @@ import axios, {
 } from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://pre-onboarding-selection-task.shop",
   timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 const logOnDev = (message: string) => {
@@ -17,27 +20,39 @@ const logOnDev = (message: string) => {
   }
 };
 
-const onError = (message: string) => {
-  alert(message);
+const onError = (status: number, message: string) => {
+  const error = { status, message };
+  throw error;
 };
 
-const onRequest = (config: AxiosRequestConfig): Promise<InternalAxiosRequestConfig> => {
-  if (!config.data) {
-    return Promise.reject(new Error("Request data is missing"));
-  }
-  const token: string | null = localStorage.getItem("token");
-  const { method, url } = config;
-  if (!config.headers) {
-    config.headers = {};
-  }
+const onRequest = (
+  config: AxiosRequestConfig
+): Promise<InternalAxiosRequestConfig> => {
+  if (!config.data) return Promise.reject(new Error("요청 데이터가 없습니다"));
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+  const token = localStorage.getItem("token");
+  const { method, url, headers = {} } = config;
+
+  headers.Authorization = token ? `Bearer ${token}` : "";
 
   logOnDev(`[API] ${method?.toUpperCase()} ${url} | Request`);
 
-  return Promise.resolve(config as InternalAxiosRequestConfig);
+  return Promise.resolve({ ...config, headers } as InternalAxiosRequestConfig);
+};
+
+const onErrorRequest = (error: AxiosError<AxiosRequestConfig>) => {
+  switch (true) {
+    case Boolean(error.config):
+      console.log("에러: 요청 실패", error);
+      break;
+    case Boolean(error.request):
+      console.log("에러: 응답 없음", error);
+      break;
+    default:
+      console.log("에러:", error);
+      break;
+  }
+  return Promise.reject(error);
 };
 
 const onResponse = (response: AxiosResponse): AxiosResponse => {
@@ -60,36 +75,42 @@ const onErrorResponse = (error: AxiosError | Error) => {
     );
 
     switch (status) {
+      case 400:
+        onError(status, "잘못된 요청입니다.");
+        break;
       case 401: {
-        console.log("ffff");
+        onError(status, "인증 실패입니다.");
         break;
       }
       case 403: {
-        onError("권환이 없음");
+        onError(status, "권한이 없습니다.");
         break;
       }
       case 404: {
-        onError("잘못된 요청");
+        onError(status, "찾을 수 없는 페이지입니다.");
         break;
       }
       case 500: {
-        onError("서버에 문제 발생");
+        onError(status, "서버 오류입니다.");
         break;
       }
       default: {
-        onError("알 수 없는 오류 발생");
+        onError(status, `에러가 발생했습니다. ${error.message}`);
       }
     }
+  } else if (error instanceof Error && error.name === "TimeoutError") {
+    logOnDev(`[API] | TimeError ${error.toString()}`);
+    onError(0, "요청 시간이 초과되었습니다.");
   } else {
-    logOnDev(`[API] | Error ${error.message}`);
-    onError(error.message);
+    logOnDev(`[API] | Error ${error.toString()}`);
+    onError(0, `에러가 발생했습니다. ${error.toString()}`);
   }
 
   return Promise.reject(error);
 };
 
 const setupInterceptors = (axiosInstance: AxiosInstance): AxiosInstance => {
-  axiosInstance.interceptors.request.use(onRequest);
+  axiosInstance.interceptors.request.use(onRequest, onErrorRequest);
   axiosInstance.interceptors.response.use(onResponse, onErrorResponse);
 
   return axiosInstance;
